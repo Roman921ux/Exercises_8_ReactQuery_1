287ed7081807fa0cc32b7263fde1a1a685561269
Skorohod Roman <skorohodroman921@gmail.com>
2024-06-25 15:42:55 +0300

    Version_1.3

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 function HomePage() {
   return (
     <Container>
-      HomePage
+      <h2>Задание_8 - ветка - reactAdmin_1</h2>
+      {/* <h2>Задание_8_Подзадача - ветка - reactAdmin_2</h2> */}
     </Container>
   );
 }
